f54b397a8a05ef9780d9f17cd43f6b4b05fd4db3
balandan <danutbalan85@yahoo.com>
2018-03-21 09:57:27 +0200

    Din class in java inapoi

---
+++ Persoana1.java
@@ -0,0 +1,50 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/21/2018 9:54:43 AM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Persoana.java
+
+
+public class Persoana
+{
+
+    public Persoana()
+    {
+    }
+
+    public void setName()
+    {
+    }
+
+    public void getName()
+    {
+    }
+
+    public void setEmail()
+    {
+    }
+
+    public void getEmai()
+    {
+    }
+
+    public void setAdresa()
+    {
+    }
+
+    public void getAdresa()
+    {
+    }
+
+    public void setPhone()
+    {
+    }
+
+    public void getNumber()
+    {
+    }
+
+    public String name;
+    public String email;
+    public String adresa;
+    public String phoneNumber;
+}
